a58be85bee9e2e0c3fdb79a67ca725bf8d5949e8
ARYAN PRIMTA <92309997+PRIMTAA@users.noreply.github.com>
2023-07-18 09:53:16 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BookmarkBorderIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
